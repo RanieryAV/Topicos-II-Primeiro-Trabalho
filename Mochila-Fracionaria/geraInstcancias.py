5e74eef83c1ab4e3ca3e4a241df5a3eb6ced8d04
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2023-10-01 12:31:18 -0300

    update files

--- Mochila-Fracionaria/geraInstcancias.py
+++ Mochila-Fracionaria/geraInstcancias.py
@@ -4,13 +4,17 @@
 
 import random
 
+tamanho = 100
 
 for i in range(1,10):
     file = open('{}.txt'.format(i),'w')
-    for j in range(i*10):
+    W = random.randint(1,100)
+    file.writelines(str(W) + " \n")
+    for j in range(tamanho):
         lucro = random.randint(1, 100)
         peso = random.randint(1, 100)
-        item = [str(lucro), ', ', str(peso), "\n"]
+        item = [str(lucro), ', ', str(peso)]
         file.writelines(item)
-    print("Instância: {}".format(i))	
+    print("Instância: {}".format(i))
+    tamanho *= 10
     file.close()
